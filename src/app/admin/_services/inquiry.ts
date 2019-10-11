2a4c4d6f8f864596205389ae34a871bd615c16f1
raymund <raymundcuizon@gmail.com>
2019-10-11 19:23:30 +0800

    inquiry

--- src/app/admin/_services/inquiry.ts
+++ src/app/admin/_services/inquiry.ts
@@ -3,13 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { APP_CONFIG, AppConfig } from '../../app-config.module';
+import { InquiryDatalist, InquiryPageSetting, InquiryPagination } from '../data/schema';
+import * as $ from 'jquery';
 
 @Injectable({ providedIn: 'root' })
 export class InquiryService { 
 
+    inqDataList: InquiryDatalist[];
+    inqPageSetting: InquiryPageSetting = {
+        page: 1,
+        paginate: 10
+    };
+    inqPagination: InquiryPagination;
+
+    headElements: ['ID', 'Title', 'Question', 'Answer', 'Actions']
+
+
     constructor(private http: HttpClient, @Inject(APP_CONFIG) private config: AppConfig
     ) {
 
+    }
+
+    getList() {
+
+        this.http.get(`${this.config.apiUrl}/private/inquiries?${$.param(this.inqPageSetting)}`)
+            .toPromise().then(res => {
+                const d:any = res;
+                this.inqDataList = d.data_list as InquiryDatalist[];
+                this.inqPagination = d.pagination;
+            })
     }
 
 }
